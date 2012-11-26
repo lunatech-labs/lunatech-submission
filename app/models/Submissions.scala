c94eb7ea5606f9137a64e0e957daa7398b9669b0
Peter Hilton <pedro+git@lunatech.com>
2012-11-26 17:50:49 +0100

    Add JSON export

--- app/models/Submissions.scala
+++ app/models/Submissions.scala
@@ -4,6 +4,9 @@ import journal.io.api.Journal
 import journal.io.api.Journal.{ReadType, WriteType}
 import play.api.Play
 import play.api.Play.current
+import play.api.libs.iteratee.Enumerator
+import java.io.InputStream
+import play.api.libs.concurrent.Promise
 
 /**
  * Journal.IO file-based persistence, using a singleton thread safe journal instance,
@@ -11,10 +14,11 @@ import play.api.Play.current
  */
 object Submissions {
 
-  val JOURNAL_DIRECTORY = "data"
+  val JournalDirectory = "data"
+  val RecentPageSize = 20
 
   val journal = new Journal()
-  journal.setDirectory(Play getFile JOURNAL_DIRECTORY)
+  journal.setDirectory(Play getFile JournalDirectory)
   journal.open()
 
   def close() {
@@ -31,10 +35,28 @@ object Submissions {
   /**
    * Read journal entries, most recent first.
    */
-  def list: Iterable[Array[Byte]] = {
+  def recent: Iterable[Array[Byte]] = {
     import scala.collection.JavaConversions._
-    journal.undo map { location =>
+    journal.undo.take(RecentPageSize) map { location =>
       journal.read(location, ReadType.SYNC)
     }
   }
+
+  /**
+   * An Enumerator that reads all journal entries, used for streaming output.
+   */
+  def all:Enumerator[Array[Byte]]  = {
+    val submissions = journal.redo().iterator()
+
+    Enumerator.fromCallback[Array[Byte]] (() => {
+      val submission = if (submissions.hasNext) {
+        val location = submissions.next()
+        Some(journal.read(location, ReadType.SYNC))
+      } else {
+        None
+      }
+      Promise.pure(submission)
+    })
+  }
+
 }
