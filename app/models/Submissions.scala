5ca278320f00fa110b7e92065211ae8ed65a062d
Peter Hilton <pedro+git@lunatech.com>
2012-11-27 09:20:00 +0100

    Fix HTTP response stream chunk size

--- app/models/Submissions.scala
+++ app/models/Submissions.scala
@@ -5,8 +5,8 @@ import journal.io.api.Journal.{ReadType, WriteType}
 import play.api.Play
 import play.api.Play.current
 import play.api.libs.iteratee.Enumerator
-import java.io.InputStream
 import play.api.libs.concurrent.Promise
+import java.io.ByteArrayOutputStream
 
 /**
  * Journal.IO file-based persistence, using a singleton thread safe journal instance,
@@ -16,6 +16,7 @@ object Submissions {
 
   val JournalDirectory = "data"
   val RecentPageSize = 20
+  val MinChunkSizeBytes = 8 * 1024
 
   val journal = new Journal()
   journal.setDirectory(Play getFile JournalDirectory)
@@ -44,14 +45,22 @@ object Submissions {
 
   /**
    * An Enumerator that reads all journal entries, used for streaming output.
+   * Read multiple records per chunk to improve performance.
    */
   def all:Enumerator[Array[Byte]]  = {
     val submissions = journal.redo().iterator()
 
     Enumerator.fromCallback[Array[Byte]] (() => {
       val submission = if (submissions.hasNext) {
-        val location = submissions.next()
-        Some(journal.read(location, ReadType.SYNC))
+        val buffer = new ByteArrayOutputStream()
+        var bufferSize = 0
+        while (submissions.hasNext && bufferSize < MinChunkSizeBytes) {
+          val location = submissions.next()
+          val record = journal.read(location, ReadType.ASYNC)
+          buffer.write(record)
+          bufferSize = bufferSize + record.length
+        }
+        Some(buffer.toByteArray)
       } else {
         None
       }
